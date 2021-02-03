7cb917ddc9018e372fc185f0cd5a9212a7e74255
Puwa <45455232+pooripro@users.noreply.github.com>
2021-02-03 10:48:26 +0700

    [UPDATE] Lab my profile

---
+++ 62130500075_groupwork_2/profile.js
@@ -0,0 +1,14 @@
+    const app = new Vue({
+        el: '#app',
+        data: {
+            firstName: 'Puwa',
+            lastName: 'Termnuphan',
+            url: './images/me.jpg',
+            career: 'Data Science',
+            article: '12',
+            followers: '603',
+            rating: '8.9'
+        }
+    });
+
+    // mounteApp = Vue.createApp(app).mount('#app')
